ccc1e82f07c77b6254a0131dcadda9ac3e3a3329
mrmedjbour <samanthabladus@gmail.com>
2020-04-30 19:01:04 +0100

    Deleting Items

--- App.js
+++ App.js
@@ -7,13 +7,19 @@ export default function App() {
   const [goals,setGoals] = useState([]);
 
   const addGoalHandler = goalValue => {
-      setGoals(currentGoals => [...goals, { key: Math.random().toString(), value: goalValue}]);
+      setGoals(currentGoals => [...goals, { key: Math.random().toString(), value: goalValue }]);
+  };
+
+  const removeGoalHandler = goalId => {
+      setGoals(currentGoals => {
+          return goals.filter((goal) => goal.key !== goalId);
+      });
   };
 
   return (
     <View style={styles.container}>
         <GoalInput onAddGoal={addGoalHandler} />
-        <FlatList data={goals} renderItem={ itemData => (<GoalItem title={itemData.item.value} />)} />
+        <FlatList data={goals} renderItem={ itemData => (<GoalItem id={itemData.item.key} title={itemData.item.value} onDelete={removeGoalHandler} />)} />
     </View>
   );
 }
--- comp/GoalItem.js
+++ comp/GoalItem.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Text, View, StyleSheet} from "react-native";
+import {Text, View, StyleSheet, TouchableOpacity} from "react-native";
 
 const GoalItem = props => {
 
@@ -8,9 +8,11 @@ const GoalItem = props => {
     };
 
     return (
-        <View style={styles.listItem}>
-            <Text>{props.title}</Text>
-        </View>
+        <TouchableOpacity onPress={props.onDelete.bind(this, props.id)} activeOpacity={0.7}>
+            <View style={styles.listItem}>
+                <Text>{props.title}</Text>
+            </View>
+        </TouchableOpacity>
     );
 };
 
