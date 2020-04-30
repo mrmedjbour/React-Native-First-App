64ffc25aadd3b434b1ca7ac914dfa2d11425b54e
Medjbour <na.medjbour@gmail.com>
2020-04-30 17:23:32 +0100

    add function

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
+  const [newGoal,setNewGoal] = useState('');
+  const [goals,setGoals] = useState([]);
+  const goalInputHandler = (curentInputText) => {
+      setNewGoal(curentInputText);
+  };
+  const addGoalHandler = () => {
+      setGoals(currentGoals => [...goals, newGoal]);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.containerInput}>
-        <TextInput placeholder="Your Goal" style={styles.textInput} />
-        <Button title="Add"/>
+        <TextInput placeholder="Your Goal" style={styles.textInput} onChangeText={goalInputHandler} value={newGoal} />
+        <Button title="Add" onPress={addGoalHandler} />
       </View>
       <View>
       </View>
@@ -21,6 +29,7 @@ const styles = StyleSheet.create({
   textInput: {
     borderBottomColor: 'black',
     borderBottomWidth: 1,
+    width: '80%',
   },
   containerInput: {
     flexDirection: 'row',
